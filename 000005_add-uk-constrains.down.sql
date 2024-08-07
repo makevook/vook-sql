103c3fb74df660b2cd6f36d2c59b42eb5ebbbf9f
seungyeop-lee <xbeast7@gmail.com>
2024-08-07 14:14:07 +0900

    vocabulary_user_uid는 유니크가 아님

--- 000005_add-uk-constrains.down.sql
+++ 000005_add-uk-constrains.down.sql
@@ -1,4 +1,4 @@
-ALTER TABLE users DROP CONSTRAINT uk_users_uid;
-ALTER TABLE term DROP CONSTRAINT uk_term_uid;
-ALTER TABLE vocabulary DROP CONSTRAINT uk_vocabulary_uid;
-ALTER TABLE vocabulary DROP CONSTRAINT uk_vocabulary_user_uid;
+ALTER TABLE users DROP CONSTRAINT IF EXISTS uk_users_uid;
+ALTER TABLE term DROP CONSTRAINT IF EXISTS uk_term_uid;
+ALTER TABLE vocabulary DROP CONSTRAINT IF EXISTS uk_vocabulary_uid;
+DROP INDEX IF EXISTS idx_vocabulary_user_uid ON vocabulary;
